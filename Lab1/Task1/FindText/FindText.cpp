67fc5892edd44a93ff4e0bb439dd5eb991cffbe8
Vishenka4ka <77840651+Vishenka4ka@users.noreply.github.com>
2022-02-21 01:23:26 +0300

    добавлен уровень абстракций

--- Lab1/Task1/FindText/FindText.cpp
+++ Lab1/Task1/FindText/FindText.cpp
@@ -13,44 +13,100 @@ struct Args
 	string searchingText;
 };
 
-optional<Args> ParseArgs(int argc, char* argv[])
+using ArgumentsCheckCallback = function<void(int argc, string SearchingString)>;
+
+optional<Args> ParseArgs(
+	int argc, 
+	char* argv[], 
+	const ArgumentsCheckCallback& errorsCallback)
 {
-	if (argc != 3)
+	if (argc != 3 || argv[2] == "")
 	{
-		cout << "Invalid arguments count\n";
-		cout << "Usage: CopiFile.exe <input file name> <text to search>\n";
+		errorsCallback(argc, argv[2]);
 		return nullopt;
 	}
+
 	Args args;
 	args.inputFileName = argv[1];
 	args.searchingText = argv[2];
 	return args;
 }
 
-using FindStringCallback = function<void(int lineIndex)>;
+void ArgumentsErrors(int argc, string searchingString)
+{
+	if (argc != 3)
+	{
+		cout << "Invalid arguments count\n";
+		cout << "Usage: CopiFile.exe <input file name> <text to search>" << endl;
+	}
 
-bool FindStingInStream(
-	istream& haystack,
-	const string& needle,
-	const FindStringCallback& callback)
+	if (searchingString == "") 
+	{
+		cout << "Invalid arguments\n";
+		cout << "Searching string can't be empty string" << endl;
+	}
+};
+
+using InputFileErrorsCallback = function<void(ifstream& input, const string inputFileName)>;
+
+bool InputFileErrorsCheck(
+	ifstream& input, 
+	const string inputFileName,
+	const InputFileErrorsCallback& errorsCallback)
+{
+	bool errors = false;
+	if (!input.is_open() || input.bad())
+	{
+		errorsCallback(input, inputFileName);
+		errors = true;
+	}
+
+	return errors;
+}
+
+void PrintInputFileErrors(ifstream& input, const string inputFileName)
+{
+	if (!input.is_open())
+	{
+		cout << "Failed to open '" << inputFileName << "' for reading" << endl;
+	}
+
+	if (input.bad())
+	{
+		cout << "Failed to read data from input file" << endl;
+	}
+}
+
+using FindStringCallback = function<void(int lineIndex)>;
+using DontFindStringCallback = function<void()>;
+
+void FindStingInStream(
+	istream& input,
+	const string& SearchingString,
+	const FindStringCallback& foundCallback, 
+	const DontFindStringCallback& didntFoundCallback)
 {
 	string line;
 	bool found = false;
-	for (int lineIndex = 1; getline(haystack, line); ++lineIndex)
+	for (int lineIndex = 1; getline(input, line); ++lineIndex)
 	{
-		auto pos = line.find(needle);
+		auto pos = line.find(SearchingString);
 		if (pos != string::npos)
 		{
 			found = true;
 			// Передаем в функцию обратного вызова информацию о
 			// первом найденном вхождении подстроки
-			if (callback)
+			if (foundCallback)
 			{
-				callback(lineIndex);
+				foundCallback(lineIndex);
 			}
 		}
 	}
-	return found;
+
+	if (!found)
+	{
+		didntFoundCallback();
+	}
 }
 
 void PrintFoundLineIndex(int lineIndex)
@@ -58,34 +114,28 @@ void PrintFoundLineIndex(int lineIndex)
 	cout << lineIndex << endl;
 }
 
+void PrintMassageIfNoStringFound()
+{
+	cout << "No string found" << endl;
+}
+
 int main(int argc, char* argv[])
 {
-	auto args = ParseArgs(argc, argv);
-	// Провекра корректности аргументов командной строки
-	if (!args)
-	{
-		return 1;
-	}
+	auto args = ParseArgs(argc, argv, ArgumentsErrors);
 
 	// Открываем входной файл
 	ifstream input;
 	input.open(args->inputFileName);
-	if (!input.is_open())
+
+	// Проверяем, не возникают ли ошибки при открытии файла 
+	// или считывании из него информации
+	if (InputFileErrorsCheck(input, args->inputFileName, PrintInputFileErrors))
 	{
-		cout << "Failed to open '" << args->inputFileName << "' for reading\n";
 		return 1;
 	}
-
-	if (input.bad())
-	{
-		std::cout << "Failed to read data from input file\n";
-		return 1;
-	}
-
-	if (!FindStingInStream(input, args->searchingText, PrintFoundLineIndex))
-	{
-		cout << "No string found" << endl;
-	}
+	
+	// Выводим результат поиска строки в файле
+	FindStingInStream(input, args->searchingText, PrintFoundLineIndex, PrintMassageIfNoStringFound);
 
 	return 0;
 }
--- Lab1/Task1/FindText/Test.bat
+++ Lab1/Task1/FindText/Test.bat
@@ -15,34 +15,39 @@ REM Если отсутствует входной файл, то должна произойти ошибка
 echo Test 2 passed succsessfully
 
 REM Поиск текста в пустом файле
-%MyProgram% EmptyFile.txt "needle" > %TEMP%output.txt|| goto err
+%MyProgram% EmptyFile.txt "needle" > %TEMP%output.txt || goto err
 fc %TEMP%output.txt EmptyFile_results.txt > nul || goto err
 echo Test 3 passed succsessfully
 
 REM Поиск текста в файле, в котором содержится только искомый текст
-%MyProgram% OnlySearchingTextFile.txt "needle" > %TEMP%output.txt|| goto err
+%MyProgram% OnlySearchingTextFile.txt "needle" > %TEMP%output.txt || goto err
 fc %TEMP%output.txt OnlySearchingTextFile_results.txt > nul || goto err
 echo Test 4 passed succsessfully
 
 REM Поиск текста в файле, в котором не содержится искомого текста
-%MyProgram% FileWithoutSearchingText.txt "needle" > %TEMP%output.txt|| goto err
+%MyProgram% FileWithoutSearchingText.txt "needle" > %TEMP%output.txt || goto err
 fc %TEMP%output.txt FileWithoutSearchingText_results.txt > nul || goto err
 echo Test 5 passed succsessfully
 
 REM Поиск текста в одностроковом файле
-%MyProgram% SingleLineFile.txt "needle" > %TEMP%output.txt|| goto err
+%MyProgram% SingleLineFile.txt "needle" > %TEMP%output.txt || goto err
 fc %TEMP%output.txt SingleLineFile_results.txt > nul || goto err
 echo Test 6 passed succsessfully
 
 REM Поиск текста в многостроковом файле
-%MyProgram% MultiLineFile.txt "needle" > %TEMP%output.txt|| goto err
+%MyProgram% MultiLineFile.txt "needle" > %TEMP%output.txt || goto err
 fc %TEMP%output.txt MultiLineFile_results.txt > nul || goto err
 echo Test 7 passed succsessfully
 
 REM Поиск повторяющегося текста в одностроковом файле
-%MyProgram% RepeatedTextInLine.txt "needle" > %TEMP%output.txt|| goto err
+%MyProgram% RepeatedTextInLine.txt "needle" > %TEMP%output.txt || goto err
 fc %TEMP%output.txt RepeatedTextInLine_results.txt > nul || goto err
 echo Test 8 passed succsessfully
+
+REM Поиск пустой строки в файле
+REM %MyProgram% MultiLineFile.txt "" > %TEMP%output.txt || goto err
+REM fc %TEMP%output.txt EmptyStringFile_results.txt > nul || goto err
+REM echo Test 9 passed succsessfully
 
 echo All test passed succsessfully
 exit /B 0
