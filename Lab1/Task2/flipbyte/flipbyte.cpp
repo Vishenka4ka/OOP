73dad8bcb1db823d0ca00da2f4e564a2bb6325c1
Vishenka4ka <77840651+Vishenka4ka@users.noreply.github.com>
2022-03-16 18:13:36 +0300

    upd

--- Lab1/Task2/flipbyte/flipbyte.cpp
+++ Lab1/Task2/flipbyte/flipbyte.cpp
@@ -10,9 +10,10 @@ using namespace std;
 struct Args 
 {
 	//сохранить сразу число в нужном типе
-		uint8_t inputNumber;
+	uint8_t inputNumber;
 };
 
+//Посмотри, есть ли в сpp унаследоваться от базового исключения
 struct ParseArgsException
 {
 	string message;
@@ -27,34 +28,34 @@ optional<Args> ParseArgs(int argc, char* argv[])
 	if (argc != 2)
 	{
 		throw ParseArgsException("Invalid arguments count\nUsage: flipbyte.exe <number>");
-		return nullopt;
 	}
 	else if (!*argv[1])
 	{
 		throw ParseArgsException("Invalid arguments\nNumber can't be empty");
-		return nullopt;
 	}
 
 	Args args;
 	size_t pos{};
 
-	args.inputNumber = stoi(argv[1], &pos, 10);
+	int number = stoi(argv[1], &pos);
 
 	if (argv[1][pos] != '\0')
 	{
 		throw ParseArgsException("Number must be in decimal notation");
 	}
 	
-	if ((args.inputNumber < 0) || (args.inputNumber > 255))
+	if (number < 0 || number > 255)
 	{
 		throw ParseArgsException("Number must be in the range from 0 to 255");
 	}
+
+	args.inputNumber = number;
 
 	return args;		
 };
 
 //задать через тип uint8_t
-int ReverseNumber(uint8_t number)
+uint8_t ReverseNumber(uint8_t number)
 {
 	uint8_t changedNumber = 0;
 	for (int i = 0; i < 4; i++)
@@ -63,7 +64,7 @@ int ReverseNumber(uint8_t number)
 			(((number >> i) & 1) << (7 - i)) + 
 			(((number >> (7 - i)) & 1) << i);
 	}
-
+	
 	return changedNumber;
 };
 
@@ -72,11 +73,13 @@ int main(int argc, char* argv[])
 	try
 	{
 		auto args = ParseArgs(argc, argv);
-		cout << ReverseNumber(args->inputNumber) << "\n";
+		cout << static_cast<int>(ReverseNumber(args->inputNumber)) << endl;
+		
 	}
-	catch (ParseArgsException& e)
+	catch (ParseArgsException& error)
 	{
-		cout << e.message << endl;
+		cout << error.message << endl;
+		return 1;
 	}
  
 	return 0;
